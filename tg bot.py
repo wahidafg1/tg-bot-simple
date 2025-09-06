f9a9000c6a3a8dfcd133797794408794e0d3c5c2
tg-bot-simple <wjan5069@gmail.com>
2025-09-06 22:53:03 +0300

    bot

--- tg bot.py
+++ tg bot.py
@@ -8,12 +8,12 @@ if not TOKEN:
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
- bot.reply_to(message, " @<25F! / F2>= ?5@2O= 1>F!  0?<H< /help")
+ bot.reply_to(message, "привет я чат бот Вахида, чем могу тебе помочь? /help")
 @bot.message_handler(commands=['help'])
 def help_cmd(message):
- bot.reply_to(message, "/start 4 =0G0FP\n/help 4 ?><>IP")
+ bot.reply_to(message, "для того чтобы я смог тебе помочь напиши нашему боссу @wahidabdullahi1 /about")
 @bot.message_handler(commands=['about'])
 def aboutme(message):
- bot.reply_to(message,"@wahidabdullahi1")
+ bot.reply_to(message,"меня зовут бот, меня создал Вахид 09.09.2025, я ещё не очень крутой, но Вахид обещал, что сделает меня многофункциональным.")
 if __name__ == "__main__":
  bot.infinity_polling(skip_pending=True)
